e5aa5b553c191cfc4f093142a54dc3cf35641ea0
Silverlan <silverlann@gmail.com>
2025-08-30 09:43:42 +0200

    wip: transition to cxx-module

--- iclient/src/implementation/vector.cpp
+++ iclient/src/implementation/vector.cpp
@@ -1,7 +1,11 @@
 // SPDX-FileCopyrightText: (c) 2019 Silverlan <opensource@pragma-engine.com>
 // SPDX-License-Identifier: MIT
 
-#include "pragma/ivector.h"
+module;
+
+module pragma.iclient;
+
+import :vector;
 
 IVector3::IVector3(float _x, float _y, float _z) : x(_x), y(_y), z(_z) {}
 IVector3::IVector3() : IVector3(0.f, 0.f, 0.f) {}
